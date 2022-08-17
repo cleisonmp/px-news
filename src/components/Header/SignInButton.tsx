ea5d75f9fc0298af8298f45b15e86e68b4089b0b
cleisonmp <cleison.mp@gmail.com>
2022-08-17 11:26:59 -0300

    header styles

---
+++ src/components/Header/SignInButton.tsx
@@ -0,0 +1,7 @@
+export function SignInButton() {
+  return (
+    <button type="button" className="bg-gray-600 rounded-3xl w-56 h-12">
+      Sign in with GitHub
+    </button>
+  )
+}
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,31 @@
+import Image from 'next/image'
+import { SignInButton } from './SignInButton'
+
+export function Header() {
+  return (
+    <header className="h-20 border-b border-gray-800">
+      <div className="flex items-center justify-between max-w-6xl mx-auto px-8 mb-40 min-w-[320px] h-20">
+        <div className="flex gap-20">
+          <Image src="/images/logo.svg" alt="" width={110} height={31} />
+          <nav className="flex gap-8 h-20 ">
+            <a
+              href="#"
+              className="flex flex-col justify-between text-gray-300 hover:text-white transition-colors"
+            >
+              <span className="flex flex-1 items-center">Home</span>
+              <span className="border-2 rounded-t border-yellow-500" />
+            </a>
+            <a
+              href="#"
+              className="flex flex-col justify-between text-gray-300 hover:text-white transition-colors"
+            >
+              <span className="flex flex-1 items-center">Posts</span>
+              <span className="border-2 rounded-t border-yellow-500" />
+            </a>
+          </nav>
+        </div>
+        <SignInButton />
+      </div>
+    </header>
+  )
+}
