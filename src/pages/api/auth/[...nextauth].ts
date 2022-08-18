7734b0018ecc26e993c26d2f4fc64fb7df7368a1
cleisonmp <cleison.mp@gmail.com>
2022-08-18 14:44:29 -0300

    faunadb creation

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
 import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
+import { query } from 'faunadb'
+import { fauna } from '../../../services/fauna'
+
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
@@ -10,4 +13,24 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+  callbacks: {
+    async signIn({ user }) {
+      const { email } = user
+      // console.log(user)
+      // console.log(account)
+      // console.log(profile)
+      console.log(email)
+      // console.log(credentials)
+
+      try {
+        await fauna.query(
+          query.Create(query.Collection('users'), { data: { email } }),
+        )
+        return true
+      } catch (error) {
+        console.error(error)
+        return false
+      }
+    },
+  },
 })
