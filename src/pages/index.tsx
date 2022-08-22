f35bf89748c6372d30ae6ff93306f14aeef7aee3
cleisonmp <cleison.mp@gmail.com>
2022-08-21 23:02:35 -0300

    changed revalidation times

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -61,6 +61,6 @@ export const getStaticProps: GetStaticProps = async () => {
     props: {
       product,
     },
-    revalidate: 60 * 60 * 24, // 24 hours
+    revalidate: 10, // 24 hours
   }
 }
